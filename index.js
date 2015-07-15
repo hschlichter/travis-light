65d63ca5913d7b15765adc9a31be184712124e6a
hschlichter <henrik.schlichter@gmail.com>
2015-07-15 15:46:09 +0200

    Support for multiple bulbs.

--- index.js
+++ index.js
@@ -12,38 +12,47 @@ lifx.on('bulb', function (bulb) {
 		throw new Error('No bulb!');
 	}
 
-	if (bulb.name !== config['lifx']['bulp_name']) {
-		console.log('This wasnt the bulp we were looking for!');
-		return;
-	}
-
-	lifx.lightsColour(0x0000, 0x0000, 0x0000, 0x0000, 0x0000, bulb);
-
-	co(function *() {
-		while (true) {
-			var accessToken = yield travis.authenticate(config['github']['oauth_token']);
-			var branch = yield travis.getBranch(
-				accessToken,
-				config['github']['organization'],
-				config['github']['repository'],
-				config['github']['branch']);
-
-			if (travis.isBranchOk(branch)) {
-				lifx.lightsColour(0x5555, 0xffff, 0x5555, 0x0dac, 0x0000, bulb);
-			} else if (travis.isBranchFailed(branch)) {
-				lifx.lightsColour(0xffff, 0xffff, 0x5555, 0x0dac, 0x0000, bulb);
-			} else if (travis.isBranchBuilding(branch)) {
-				lifx.lightsColour(0x2222, 0xffff, 0x2222, 0x0000, 0x0000, bulb);
-				yield wait(500);
-				lifx.lightsColour(0x2222, 0x8888, 0x1111, 0x0000, 0x00ff, bulb);
-			} else {
-				lifx.lightsColour(0x0000, 0x0000, 0x3333, 0x0dac, 0x0000, bulb);
-			}
-
-			yield wait(500);
+	config['bulbs'].forEach(function (bulbConfig) {
+		if (bulb.name !== bulbConfig['lifx']['bulb_name']) {
+			return;
 		}
-	}).catch(function (err) {
-		throw err;
+
+		console.log(bulb);
+
+		console.log('Found bulb - ' + bulb.name);
+
+		lifx.lightsColour(0x0000, 0x0000, 0x0000, 0x0000, 0x0000, bulb);
+
+		co(function *() {
+			while (true) {
+				var accessToken = yield travis.authenticate(bulbConfig['github']['oauth_token']);
+				var branch = yield travis.getBranch(
+					accessToken,
+					bulbConfig['github']['organization'],
+					bulbConfig['github']['repository'],
+					bulbConfig['github']['branch']);
+
+				if (travis.isBranchOk(branch)) {
+					console.log('loop - ok - ' + bulb.name);
+					lifx.lightsColour(0x5555, 0xffff, 0x5555, 0x0dac, 0x0000, bulb);
+				} else if (travis.isBranchFailed(branch)) {
+					console.log('loop - failed - ' + bulb.name);
+					lifx.lightsColour(0xffff, 0xffff, 0x5555, 0x0dac, 0x0000, bulb);
+				} else if (travis.isBranchBuilding(branch)) {
+					console.log('loop - building - ' + bulb.name);
+					lifx.lightsColour(0x2222, 0xffff, 0x2222, 0x0000, 0x0000, bulb);
+					yield wait(500);
+					lifx.lightsColour(0x2222, 0x8888, 0x1111, 0x0000, 0x00ff, bulb);
+				} else {
+					console.log('loop - unknown - ' + bulb.name);
+					lifx.lightsColour(0x0000, 0x0000, 0x3333, 0x0dac, 0x0000, bulb);
+				}
+
+				yield wait(500);
+			}
+		}).catch(function (err) {
+			throw err;
+		});
 	});
 });
 
